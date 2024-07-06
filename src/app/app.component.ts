2ee77ad86d52b8596edb5978931a22fb3be52599
Nikoli <huseynlinihad4400@gmail.com>
2024-07-06 11:05:38 +0400

    Last Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as CryptoTS from 'crypto-ts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cryptographic';
+
+
+  encrypt(veri : string){
+    // var CryptoTS = require("crypto-ts");
+ 
+    // Encrypt
+    var ciphertext = CryptoTS.AES.encrypt(veri, 'secret key 123');
+
+    return ciphertext;
+  }
+
+  login(){
+    let accessToken=this.encrypt("Access-Token")
+    localStorage.setItem("token",accessToken.toString())
+  }
+
+  getToken(){
+    let decrypt=this.decrypt();
+    console.log(decrypt);
+  }
+
+  decrypt(){
+    let  veri =localStorage.getItem("token");
+    if(veri!=null){
+      var bytes  = CryptoTS.AES.decrypt(veri,'secret key 123');
+      var plaintext = bytes.toString(CryptoTS.enc.Utf8);
+   
+      return plaintext;
+    }
+    else{
+      console.error("Token değeri boş.")
+      return null;
+    }
+  }
+
 }
